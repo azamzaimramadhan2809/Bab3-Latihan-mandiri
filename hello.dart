98f6374daf266e8ff36cf857629cfd6c6c6b6f5c
azamzaimramadhan2809 <azamzaimramadhan2809@gmail.com>
2026-07-21 08:43:42 +0700

    100%

---
+++ hello.dart
@@ -0,0 +1,9 @@
+void main() {
+    const double pi = 3.14;
+
+    int jariJari = 7;
+
+    double luas = pi * jariJari * jariJari;
+
+  print("Luas lingkaran dengan jari-jari $jariJari adalah $luas");
+}
